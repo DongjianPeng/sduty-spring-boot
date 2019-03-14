4cf93d28c5485d226ea40f1591da9b22488e7d18
pengdongjian <541874767@qq.com>
2019-03-14 13:43:12 +0800

    xx

--- src/test/java/top/murphypen/test/Playground.java
+++ src/test/java/top/murphypen/test/Playground.java
@@ -4,9 +4,25 @@ import org.junit.Test;
 
 public class Playground {
 
+    public static void T(Runnable r) {
+        try {
+            r.run();
+        } catch (Exception e) {
+            System.out.println("1111111");
+            throw e;
+        } finally {
+
+        }
+    }
 
     @Test
     public void test() {
-
+        try {
+            T(() -> {
+                System.out.println(5 / 0);
+            });
+        } catch (Exception e) {
+            System.out.println("2222222");
+        }
     }
 }
